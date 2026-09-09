ee5a2e597299f27a1f982056b830b89dabefcde0
Isaías Zelaya <zisaiasemmanuel@gmail.com>
2026-09-09 11:37:19 -0600

    Add files via upload

---
+++ conversor/Conversor de Áreas/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Conversor_de_Áreas
+{
+    partial class frmConversorAreas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDe = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.cboA = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDe.Location = new System.Drawing.Point(26, 43);
+            this.lblDe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(123, 23);
+            this.lblDe.TabIndex = 0;
+            this.lblDe.Text = "Convertir de:";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(387, 43);
+            this.lblA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(112, 23);
+            this.lblA.TabIndex = 1;
+            this.lblA.Text = "Convertir a:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(42, 131);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(94, 23);
+            this.lblCantidad.TabIndex = 2;
+            this.lblCantidad.Text = "Cantidad:";
+            // 
+            // cboDe
+            // 
+            this.cboDe.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Items.AddRange(new object[] {
+            "Pies cuadrados",
+            "Varas cuadradas",
+            "Yardas cuadradas",
+            "Metros cuadrados",
+            "Tareas",
+            "Manzanas",
+            "Hectáreas"});
+            this.cboDe.Location = new System.Drawing.Point(152, 40);
+            this.cboDe.Margin = new System.Windows.Forms.Padding(4);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(209, 31);
+            this.cboDe.TabIndex = 3;
+            this.cboDe.SelectedIndexChanged += new System.EventHandler(this.cboDe_SelectedIndexChanged);
+            // 
+            // cboA
+            // 
+            this.cboA.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboA.FormattingEnabled = true;
+            this.cboA.Items.AddRange(new object[] {
+            "Pies cuadrados",
+            "Varas cuadradas",
+            "Yardas cuadradas",
+            "Metros cuadrados",
+            "Tareas",
+            "Manzanas",
+            "Hectáreas"});
+            this.cboA.Location = new System.Drawing.Point(507, 40);
+            this.cboA.Margin = new System.Windows.Forms.Padding(4);
+            this.cboA.Name = "cboA";
+            this.cboA.Size = new System.Drawing.Size(198, 31);
+            this.cboA.TabIndex = 4;
+            this.cboA.SelectedIndexChanged += new System.EventHandler(this.cboA_SelectedIndexChanged);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(144, 128);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(132, 30);
+            this.txtCantidad.TabIndex = 5;
+            this.txtCantidad.TextChanged += new System.EventHandler(this.txtCantidad_TextChanged);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(254, 201);
+            this.btnConvertir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(100, 32);
+            this.btnConvertir.TabIndex = 6;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(507, 199);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 34);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(387, 131);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(110, 23);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resultado: ";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // frmConversorAreas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 316);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cboA);
+            this.Controls.Add(this.cboDe);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblDe);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmConversorAreas";
+            this.Text = "Conversor de Áreas";
+            this.Load += new System.EventHandler(this.frmConversorAreas_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.ComboBox cboA;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ conversor/Conversor de Áreas/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor_de_Áreas
+{
+    public partial class frmConversorAreas : Form
+    {
+        // Diccionario: unidad -> factor de conversión a METROS CUADRADOS (unidad base)
+        private Dictionary<string, double> factoresAMetros = new Dictionary<string, double>
+        {
+            { "Pies Cuadrados", 0.092903 },
+            { "Varas Cuadradas", 0.698896 },
+            { "Yardas Cuadradas", 0.836127 },
+            { "Metros Cuadrados", 1.0 },
+            { "Tareas", 437.5 },
+            { "Manzanas", 6988.96 },
+            { "Hectáreas", 10000.0 }
+        };
+
+        public frmConversorAreas()
+        {
+            InitializeComponent();
+            PrepararFormulario();
+        }
+
+        private void PrepararFormulario()
+        {
+            cboDe.Items.Clear();
+            cboA.Items.Clear();
+
+            foreach (string unidad in factoresAMetros.Keys)
+            {
+                cboDe.Items.Add(unidad);
+                cboA.Items.Add(unidad);
+            }
+
+            cboDe.SelectedIndex = 0;
+            cboA.SelectedIndex = 3;
+            lblResultado.Text = "Resultado: ---";
+        }
+
+        private void frmConversorAreas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCantidad.Text))
+            {
+                MessageBox.Show("Por favor ingrese un valor a convertir.",
+                                "Atención",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                txtCantidad.Focus();
+                return;
+            }
+
+            if (!double.TryParse(txtCantidad.Text, out double valor) || valor < 0)
+            {
+                MessageBox.Show("El valor ingresado no es un número válido.",
+                                "Error de entrada",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                txtCantidad.Focus();
+                return;
+            }
+
+            if (cboDe.SelectedItem == null || cboA.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione la unidad de origen y destino.", "Atención");
+                return;
+            }
+
+            string unidadDesde = cboDe.SelectedItem.ToString();
+            string unidadHasta = cboA.SelectedItem.ToString();
+
+            // Conversión matemática directa
+            double valorEnMetros = valor * factoresAMetros[unidadDesde];
+            double resultado = valorEnMetros / factoresAMetros[unidadHasta];
+
+            lblResultado.Text = "Resultado: " + resultado.ToString("G8") + " " + unidadHasta;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Clear();
+            cboDe.SelectedIndex = 0;
+            cboA.SelectedIndex = 3;
+            lblResultado.Text = "Resultado: ---";
+            txtCantidad.Focus();
+        }
+
+        
+        private void txtCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboDe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboA_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
