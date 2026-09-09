567d539bd74ecd75897392320d12d45aff260ebc
Isaías Zelaya <zisaiasemmanuel@gmail.com>
2026-09-09 11:42:52 -0600

    Add files via upload

---
+++ impuesto/Parcial-I/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Parcial_I
+{
+    partial class frmCalculoImpuestos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.lblImpuesto = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblTramo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(45, 35);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(446, 19);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "CÁLCULO DE IMPUESTOS A ACTIVIDADES ECONÓMICAS";
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonto.Location = new System.Drawing.Point(43, 91);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(256, 19);
+            this.lblMonto.TabIndex = 1;
+            this.lblMonto.Text = "Ingrese monto de actividad económica: $";
+            this.lblMonto.Click += new System.EventHandler(this.lblMonto_Click);
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMonto.Location = new System.Drawing.Point(300, 87);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(136, 26);
+            this.txtMonto.TabIndex = 2;
+            this.txtMonto.TextChanged += new System.EventHandler(this.txtMonto_TextChanged);
+            // 
+            // lblImpuesto
+            // 
+            this.lblImpuesto.AutoSize = true;
+            this.lblImpuesto.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImpuesto.Location = new System.Drawing.Point(42, 148);
+            this.lblImpuesto.Name = "lblImpuesto";
+            this.lblImpuesto.Size = new System.Drawing.Size(128, 19);
+            this.lblImpuesto.TabIndex = 3;
+            this.lblImpuesto.Text = "Impuesto a pagar: ?";
+            this.lblImpuesto.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(54, 188);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(77, 27);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(149, 188);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(77, 27);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(400, 188);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(77, 27);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblTramo
+            // 
+            this.lblTramo.AutoSize = true;
+            this.lblTramo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTramo.Location = new System.Drawing.Point(42, 120);
+            this.lblTramo.Name = "lblTramo";
+            this.lblTramo.Size = new System.Drawing.Size(117, 19);
+            this.lblTramo.TabIndex = 7;
+            this.lblTramo.Text = "Tramo aplicado: ?";
+            // 
+            // frmCalculoImpuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 262);
+            this.Controls.Add(this.lblTramo);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblImpuesto);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmCalculoImpuestos";
+            this.Text = "Cálculo de Impuestos";
+            this.Load += new System.EventHandler(this.frmCalculoImpuestos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblMonto;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Label lblImpuesto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblTramo;
+    }
+}
+
---
+++ impuesto/Parcial-I/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_I
+{
+    public partial class frmCalculoImpuestos : Form
+    {
+        // Matriz con los datos de la tabla (Desde, Hasta, Precio, Adicional)[cite: 1]
+        double[,] tablaImpuestos = new double[,]
+        {
+            { 0.01, 500.00, 1.50, 0.00 },
+            { 500.01, 1000.00, 1.50, 3.00 },
+            { 1000.01, 2000.00, 3.00, 3.00 },
+            { 2000.01, 3000.00, 6.00, 3.00 },
+            { 3000.01, 6000.00, 9.00, 2.00 },
+            { 6000.01, 8000.00, 15.00, 2.00 },
+            { 8000.01, 18000.00, 15.00, 2.00 },
+            { 18000.01, 30000.00, 39.00, 2.00 },
+            { 30000.01, 60000.00, 63.00, 1.00 },
+            { 60000.01, 100000.00, 93.00, 0.80 },
+            { 100000.01, 200000.00, 125.00, 0.70 },
+            { 200000.01, 300000.00, 195.00, 0.60 },
+            { 300000.01, 400000.00, 255.00, 0.45 },
+            { 400000.01, 500000.00, 300.00, 0.40 },
+            { 500000.01, 1000000.00, 340.00, 0.30 },
+            { 1000000.01, 99999999.00, 490.00, 0.18 }
+        };
+
+        public frmCalculoImpuestos()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCalculoImpuestos_Load(object sender, EventArgs e)
+        {
+            // Suscripción explícita de eventos para asegurar que los botones respondan al hacer clic
+            btnCalcular.Click += new EventHandler(btnCalcular_Click);
+            btnLimpiar.Click += new EventHandler(btnLimpiar_Click);
+            btnSalir.Click += new EventHandler(btnSalir_Click);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMonto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblMonto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // --- MÉTODOS PARA NAVEGAR LOS BOTONES ---
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        // --- FUNCIONES DE LÓGICA ---
+
+        private void Calcular()
+        {
+            if (double.TryParse(txtMonto.Text, out double monto) && monto > 0)
+            {
+                int filas = tablaImpuestos.GetLength(0);
+
+                for (int i = 0; i < filas; i++)
+                {
+                    double desde = tablaImpuestos[i, 0];
+                    double hasta = tablaImpuestos[i, 1];
+                    double precio = tablaImpuestos[i, 2];
+                    double adicional = tablaImpuestos[i, 3];
+
+                    if (monto >= desde && monto <= hasta)
+                    {
+                        double sobrante = monto - desde;
+                        double total = (sobrante / 1000.0) * adicional + precio;
+
+                        lblTramo.Text = "Tramo aplicado: N° " + (i + 1) + " ($" + desde + " - $" + hasta + ")";
+                        lblImpuesto.Text = "Impuesto a pagar: $" + Math.Round(total, 2).ToString("F2");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un monto numérico válido mayor a cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Limpiar()
+        {
+            txtMonto.Clear();
+            lblTramo.Text = "Tramo aplicado: ?";
+            lblImpuesto.Text = "Impuesto a pagar: ?";
+            txtMonto.Focus();
+        }
+    }
+}
